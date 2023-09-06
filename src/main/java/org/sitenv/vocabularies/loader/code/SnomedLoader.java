147324f89ba24b3852cc3a42343142df66fbd4b4
AC097326 <antara.chakraborty@cerner.com>
2023-09-06 21:35:16 +0530

    test

--- src/main/java/org/sitenv/vocabularies/loader/code/SnomedLoader.java
+++ src/main/java/org/sitenv/vocabularies/loader/code/SnomedLoader.java
@@ -35,6 +35,7 @@ public class SnomedLoader extends BaseCodeLoader implements VocabularyLoader {
             for (File file : filesToLoad) {
                 if (file.isFile() && !file.isHidden()) {
                     logger.debug("Loading SNOMED File: " + file.getName());
+                    logger.info("testing")
                     String codeSystem = file.getParentFile().getName();
                     int count = 0;
                     fileReader = new FileReader(file);
